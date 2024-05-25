fd20fab54685ceab0ba261fe3f53874149bf0f16
Kushani Tharushika <135972088+KushaniTharushika@users.noreply.github.com>
2024-05-25 23:00:07 +0530

    Lab 04-3

---
+++ Lab 04-3/Lab 04-3/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Lab_04_3
+{
+    partial class txtDigit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDigit = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.butCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblDigit
+            // 
+            this.lblDigit.AutoSize = true;
+            this.lblDigit.Location = new System.Drawing.Point(161, 87);
+            this.lblDigit.Name = "lblDigit";
+            this.lblDigit.Size = new System.Drawing.Size(124, 13);
+            this.lblDigit.TabIndex = 0;
+            this.lblDigit.Text = "Enter Calculate Numbers";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(291, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // butCalculate
+            // 
+            this.butCalculate.Location = new System.Drawing.Point(291, 168);
+            this.butCalculate.Name = "butCalculate";
+            this.butCalculate.Size = new System.Drawing.Size(75, 23);
+            this.butCalculate.TabIndex = 2;
+            this.butCalculate.Text = "Calculate";
+            this.butCalculate.UseVisualStyleBackColor = true;
+            this.butCalculate.Click += new System.EventHandler(this.butCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(179, 279);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(37, 13);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(266, 272);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 4;
+            this.txtResult.TextChanged += new System.EventHandler(this.txtResult_TextChanged);
+            // 
+            // txtDigit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.butCalculate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblDigit);
+            this.Name = "txtDigit";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDigit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button butCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Lab 04-3/Lab 04-3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_04_3
+{
+    public partial class txtDigit : Form
+    {
+        public txtDigit()
+        {
+            InitializeComponent();
+        }
+
+        private void butCalculate_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+
+            try
+            {
+                double result = Evaluate(input);
+                txtResult.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = "Error: " + ex.Message;
+            }
+        }
+
+        private double Evaluate(string expression)
+        {
+            var dataTable = new DataTable();
+            var result = dataTable.Compute(expression, "");
+            return Convert.ToDouble(result);
+        }
+
+        private void txtResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
